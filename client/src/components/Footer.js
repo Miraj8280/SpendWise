15821819a794b7dc6406c6a5524b98ebbcc38c6c
Miraj Asraf <mirajasraf786@gmail.com>
2023-04-21 13:57:49 +0530

    footer updated

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,8 +5,7 @@ function Footer() {
     <footer className="text-center ml-4 py-3 text-gray-800 bg-gray-100 relative">
       Made with
       <span className="inline-block relative">
-        <span className="animate-pulse absolute h-full w-full top-0 left-0 transform scale-150 heart-color"> &#10084; </span>
-        <span className="relative ml-2 mr-2">💚</span>
+        <span className="relative ml-2 mr-2">💛</span>
       </span>
        by <span className="name inline-block relative text-blue-500">Miraj Asraf</span>
     </footer>
